606fca730044e44a8db91adc20e6bb33d86e23b0
MaxChuloshnikov <Maks447@ukr.net>
2023-04-18 18:02:34 +0300

    Google Auth done

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import { useSession, signIn, signOut } from "next-auth/react";
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return (
-    <>
+
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <>
       <Head>
         <title>MCH Macstore admin</title>
         <meta name="description" content="Welcome to admin panel" />
@@ -17,10 +21,24 @@ export default function Home() {
       <main>
         <div className='bg-gray-800 w-screen h-screen flex items-center'>
           <div className='text-center w-full'>
-              <button className='bg-white p-2 px-4 rounded-lg'>Login with Google</button>
+              <button 
+              onClick={() => signIn('google')}
+              className='bg-white p-2 px-4 rounded-lg text-base font-semibold'>
+                Login with Google
+              </button>
           </div>
         </div>
       </main>
     </>
-  )
-}
+    );
+
+  } 
+   return (
+    <div>
+      logged in {session.user.email}
+    </div>
+   )
+  }
+
+  
+
