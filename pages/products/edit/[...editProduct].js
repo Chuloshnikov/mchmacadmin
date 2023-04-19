91fda050099104ba0822009d0ef2dd84474d81c7
MaxChuloshnikov <Maks447@ukr.net>
2023-04-19 19:56:29 +0300

    ProductPage done

---
+++ pages/products/edit/[...editProduct].js
@@ -0,0 +1,16 @@
+import Layout from '@/components/Layout';
+import React from 'react';
+
+const EditProductPage = () => {
+
+    const router = useRouter();
+    console.log(router);
+
+  return (
+    <Layout>
+        Edit product form here
+    </Layout>
+  )
+}
+
+export default EditProductPage;
